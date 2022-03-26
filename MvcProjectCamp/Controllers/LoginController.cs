510260ded0e3402af1090ed2b4a73fd937672a4f
Oznur Gur <Oznur.Gur@logo.com.tr>
2022-03-27 00:02:04 +0300

    ....

---
+++ MvcProjectCamp/Controllers/LoginController.cs
@@ -0,0 +1,38 @@
+﻿using DataAccessLayer.Concrete;
+using EntityLayer.Concrete;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MvcProjectCamp.Controllers
+{
+    public class LoginController : Controller
+    {
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public ActionResult Index(Admin admin)
+        {
+            Context context = new Context();
+            var adminuserinfo = context.Admins.
+                FirstOrDefault(x =>
+                x.AdminUserName == admin.AdminUserName &&
+                x.AdminPassword == admin.AdminPassword);
+            if (adminuserinfo != null)
+            {
+                return RedirectToAction("Index", "AdminCategory");
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+
+        }
+    }
+}
